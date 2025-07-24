779eeade4df6b7bf4f4d6f9254270b74dda9f6c4
wertezaj <99995054+wertezaj@users.noreply.github.com>
2025-07-24 09:19:07 +0200

    wsad

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <ul>
+                    <li>
+                        <a href="index.html">index</a>
+                        <a href="species.html">species</a>
+                    </li>
+                </ul>
+            </header>
+        </>
+    )
+}
+
+export default Header
